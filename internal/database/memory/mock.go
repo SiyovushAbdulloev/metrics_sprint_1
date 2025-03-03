235cdb3b0f7c8aff3b8eba721edd02f1d3251164
Asjica <siyovush.2000.abdulloev>
2025-03-03 08:18:45 +0500

    Added test for server

---
+++ internal/database/memory/mock.go
@@ -0,0 +1,79 @@
+package memory
+
+import (
+	"github.com/SiyovushAbdulloev/metriks_sprint_1/internal/models"
+)
+
+type MockMemStorage struct {
+	data []models.Metric
+}
+
+type MockMetricStorage struct {
+	DB MockMemStorage
+}
+
+func NewMockMetricStorage(db MockMemStorage) MockMetricStorage {
+	return MockMetricStorage{
+		DB: db,
+	}
+}
+
+func NewMockDB(data []models.Metric) MockMemStorage {
+	return MockMemStorage{
+		data: data,
+	}
+}
+
+func (mms *MockMetricStorage) StoreMetric(metric models.Metric) bool {
+	switch metric.Type {
+	case models.Gauge:
+		data := mms.DB.data
+		if len(data) == 0 {
+			data = append(data, metric)
+		} else {
+			notFound := true
+			for k, v := range data {
+				if v.Name == metric.Name {
+					notFound = false
+					data[k] = metric
+				}
+			}
+
+			if notFound {
+				data = append(data, metric)
+			}
+		}
+
+		mms.DB.data = data
+		return true
+	case models.Counter:
+		data := mms.DB.data
+
+		if len(data) == 0 {
+			data = append(data, metric)
+		} else {
+			notFound := true
+			for k, v := range data {
+				if v.Name == metric.Name {
+					notFound = false
+					newValue := metric.Value.(int64)
+					previousValue := v.Value.(int64)
+					data[k] = models.Metric{
+						Name:  metric.Name,
+						Type:  metric.Type,
+						Value: newValue + previousValue,
+					}
+				}
+			}
+
+			if notFound {
+				data = append(data, metric)
+			}
+		}
+
+		mms.DB.data = data
+		return true
+	default:
+		return false
+	}
+}
---
+++ internal/transport/rest/handlers_test.go
@@ -0,0 +1,114 @@
+package rest
+
+import (
+	"fmt"
+	"github.com/SiyovushAbdulloev/metriks_sprint_1/internal/database/memory"
+	"github.com/SiyovushAbdulloev/metriks_sprint_1/internal/models"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestServer_StoreMetric(t *testing.T) {
+	type want struct {
+		Code        int
+		Response    string
+		ContentType string
+	}
+	type Metric struct {
+		Type  string
+		Name  string
+		Value any
+	}
+	tests := []struct {
+		name   string
+		metric Metric
+		want   want
+	}{
+		{
+			name: "Storing alloc",
+			metric: Metric{
+				Type:  string(models.Gauge),
+				Name:  "alloc",
+				Value: 1234.32,
+			},
+			want: want{
+				Code:        http.StatusOK,
+				Response:    "OK",
+				ContentType: "text/plain",
+			},
+		},
+		{
+			name: "Storing counter",
+			metric: Metric{
+				Type:  string(models.Counter),
+				Name:  "counter",
+				Value: 12,
+			},
+			want: want{
+				Code:        http.StatusOK,
+				Response:    "OK",
+				ContentType: "text/plain",
+			},
+		},
+		{
+			name: "Storing unknown type",
+			metric: Metric{
+				Type:  "counter_2",
+				Name:  "anything",
+				Value: 12,
+			},
+			want: want{
+				Code:        http.StatusBadRequest,
+				Response:    "invalid type\n",
+				ContentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name: "Storing invalid value",
+			metric: Metric{
+				Type:  string(models.Counter),
+				Name:  "counter",
+				Value: "12asdf",
+			},
+			want: want{
+				Code:        http.StatusBadRequest,
+				Response:    "invalid value\n",
+				ContentType: "text/plain; charset=utf-8",
+			},
+		},
+	}
+
+	db := memory.NewMockDB(make([]models.Metric, 0))
+	mockMetricStorage := memory.NewMockMetricStorage(db)
+	server := InitApp(&mockMetricStorage)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			addr := fmt.Sprintf("/update/%s/%s/%v", tt.metric.Type, tt.metric.Name, tt.metric.Value)
+			fmt.Printf("Address: %s\n", addr)
+			request := httptest.NewRequest(http.MethodPost, addr, nil)
+			w := httptest.NewRecorder()
+
+			mux := http.NewServeMux()
+			mux.HandleFunc("POST /update/{type}/{name}/{value}", server.StoreMetric)
+			mux.ServeHTTP(w, request)
+			//server.StoreMetric(w, request)
+
+			res := w.Result()
+
+			assert.Equal(t, tt.want.Code, res.StatusCode)
+
+			defer res.Body.Close()
+
+			resBody, err := io.ReadAll(res.Body)
+
+			require.NoError(t, err)
+			assert.Equal(t, tt.want.Response, string(resBody))
+			assert.Equal(t, tt.want.ContentType, res.Header.Get("Content-Type"))
+		})
+	}
+}
